d420a20d7bf0c6608caebb343fc287b401c290e2
600020344 <77069279+600020344@users.noreply.github.com>
2021-02-22 09:13:00 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,5 +3,9 @@ class Main {
     Cow c = new Cow();
 System.out.println( c.getType() + " goes " + c.getSound() );
 // < your code here >
+    Chick ch = new Chick();
+System.out.println( ch.getType() + " goes " + ch.getSound() );
+    Pig p = new Pig();
+System.out.println( p.getType() + " goes " + p.getSound() );
   }
 }
